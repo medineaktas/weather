9c77900fc46ef4ec6b2c56b4b209a623110991bb
medineaktas <medneaktas@gmail.com>
2023-06-29 16:55:50 +0300

    added spring security

--- src/main/java/com/example/weather/WeatherApplication.java
+++ src/main/java/com/example/weather/WeatherApplication.java
@@ -1,8 +1,11 @@
 package com.example.weather;
 
+import com.example.weather.dto.RegisterRequest;
+import com.example.weather.service.AuthenticationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,6 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Clock;
+
+import static com.example.weather.model.user.Role.ADMIN;
+import static com.example.weather.model.user.Role.MANAGER;
 
 
 @SpringBootApplication
@@ -20,6 +26,32 @@ public class WeatherApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};}
 	@Bean
 	public ObjectMapper objectMapper() {
 		ObjectMapper objectMapper = new ObjectMapper();
---
+++ src/main/java/com/example/weather/model/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.example.weather.model.token;
+
+public enum TokenType {
+    BEARER
+}
